7306bcd852001127fb93c35ce913f3200a686351
Pero <g00422221@atu.ie>
2023-10-23 16:26:29 +0100

    added person class

---
+++ src/main/java/ie/atu/Person.java
@@ -0,0 +1,23 @@
+package ie.atu;
+
+public class Person {
+
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+}
+
+    public Person() {
+    }
+
+
+    public Person(String firstName, String lastName, int age)
+    {
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age = age;
+
+
+    }
